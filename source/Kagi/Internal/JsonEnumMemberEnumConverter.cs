a9376426bc3f3213771d5898722987582b90e075
Patchouli <self@patchouli.sh>
2024-11-01 21:27:57 -0400

    Added some basic serialization scaffholding code

---
+++ source/Kagi/Internal/JsonEnumMemberEnumConverter.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Runtime;
+using System.Runtime.Serialization;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Kagi.Internal
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <typeparam name="TEnum"></typeparam>
+	internal sealed class JsonEnumMemberEnumConverter<TEnum> :
+		JsonConverter<TEnum>
+			where TEnum :
+				struct,
+				Enum
+	{
+		private readonly IDictionary<TEnum, string> jsonValues;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public JsonEnumMemberEnumConverter() :
+			base()
+		{
+			this.jsonValues =
+				new Dictionary<TEnum, string>();
+
+			foreach (TEnum value in Enum.GetValues(typeof(TEnum)))
+			{
+				this.jsonValues[value] =
+					typeof(TEnum)
+						.GetMember(value.ToString())
+						.FirstOrDefault()?
+						.GetCustomAttribute<EnumMemberAttribute>()?
+						.Value;
+			}
+		}
+
+		/// <inheritdoc />
+		public sealed override bool CanConvert(
+			Type typeToConvert) =>
+				(typeToConvert == typeof(TEnum));
+
+		/// <inheritdoc />
+		public override TEnum Read(
+			ref Utf8JsonReader reader,
+			Type typeToConvert,
+			JsonSerializerOptions options)
+		{
+			var jsonValue =
+				reader.GetString();
+
+			foreach (var kvp in this.jsonValues)
+			{
+				if (kvp.Value.Equals(
+					jsonValue,
+					StringComparison.OrdinalIgnoreCase))
+				{
+					return kvp.Key;
+				}
+			}
+
+			throw new JsonException(
+				$"Unable to convert \"{jsonValue}\" to {typeof(TEnum)}.");
+		}
+
+		/// <inheritdoc />
+		public override void Write(
+			Utf8JsonWriter writer,
+			TEnum value,
+			JsonSerializerOptions options)
+		{
+			if (this.jsonValues
+				.TryGetValue(
+					value,
+					out var jsonValue))
+			{
+				writer.WriteStringValue(
+					jsonValue);
+			}
+			else
+			{
+				throw new JsonException(
+					$"Unable to convert \"{value}\" to JSON.");
+			}
+		}
+	}
+}
---
+++ source/Kagi/KagiUrlHelper.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Kagi
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public static class KagiUrlHelper
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		private const string ImageProxyPathFragment =
+			"/proxy";
+
+		/// <summary>
+		/// 
+		/// </summary>
+		private const string ImageProxyBaseUrlValue =
+			"https://kagi.com/";
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public static Uri ImageProxyBaseUrl { get; } =
+			new Uri(ImageProxyBaseUrlValue);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="value"></param>
+		/// <returns></returns>
+		public static bool IsImageProxyPathFragment(
+			string value)
+		{
+			ArgumentNullException
+				.ThrowIfNull(
+					value,
+					nameof(value));
+
+			return value
+				.StartsWith(
+					ImageProxyPathFragment);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="value"></param>
+		/// <returns></returns>
+		/// <exception cref="InvalidOperationException"></exception>
+		public static Uri ToImageProxyUrl(
+			string value)
+		{
+			ArgumentNullException
+				.ThrowIfNull(
+					value,
+					nameof(value));
+
+			if (Uri.TryCreate(
+					ImageProxyBaseUrl,
+					value.TrimStart('/'),
+					out var result))
+			{
+				return result;
+			}
+			else
+			{
+				throw new InvalidOperationException(
+					$"Failed to create a valid image proxy URL.");
+			}
+		}
+	}
+}
