dd061770da42d627404bffb3cc5ba6bcdfea0ff9
Patchouli <self@patchouli.sh>
2024-11-02 12:58:25 -0400

    Added more unit tests

--- source/Kagi.Tests/KagiClientTests.Answer.cs
+++ source/Kagi.Tests/KagiClientTests.Answer.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
 
 using Microsoft;
 using Microsoft.VisualStudio;
@@ -14,17 +16,55 @@ namespace Kagi
 		/// <summary>
 		/// 
 		/// </summary>
+		public static IEnumerable<object[]> AnswerThrowOnBadRequestData =>
+			[
+				[
+					new KagiAnswerOptions()
+					{
+						Query = default,
+					}
+				],
+				[
+					new KagiAnswerOptions()
+					{
+						Query = String.Empty,
+					}
+				],
+			];
+
+		/// <summary>
+		/// 
+		/// </summary>
 		/// <returns></returns>
-		[TestMethod]
 		[TestCategory(
 			"Answer")]
 		[TestCategory(
 			"Argument")]
+		[TestMethod]
 		public async Task AnswerThrowOnArgumentNullAsync()
 		{
 			await Assert.ThrowsExceptionAsync<ArgumentNullException>(
 				() => this.client.AnswerAsync(
 					default));
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="options"></param>
+		/// <returns></returns>
+		[TestCategory(
+			"Answer")]
+		[TestCategory(
+			"BadRequest")]
+		[DataTestMethod]
+		[DynamicData(nameof(AnswerThrowOnBadRequestData))]
+		public async Task AnswerThrowOnBadRequestAsync(
+			KagiAnswerOptions options)
+		{
+			await Assert.ThrowsExceptionAsync<KagiException>(
+				() => this.client.AnswerAsync(
+					options));
 		}
 	}
 }
--- source/Kagi.Tests/KagiClientTests.Summarize.cs
+++ source/Kagi.Tests/KagiClientTests.Summarize.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
 
 using Microsoft;
 using Microsoft.VisualStudio;
@@ -14,17 +16,64 @@ namespace Kagi
 		/// <summary>
 		/// 
 		/// </summary>
+		public static IEnumerable<object[]> SummarizeThrowOnBadRequestData =>
+			[
+				[
+					new KagiSummarizeOptions()
+					{
+						Url = default,
+						Text = default,
+					}
+				],
+				[
+					new KagiSummarizeOptions()
+					{
+						Url = default,
+						Text = String.Empty,
+					}
+				],
+				[
+					new KagiSummarizeOptions()
+					{
+						Url = new Uri("http://www.example.test"),
+						Text = "Example Text",
+					}
+				],
+			];
+
+		/// <summary>
+		/// 
+		/// </summary>
 		/// <returns></returns>
-		[TestMethod]
 		[TestCategory(
 			"Summarize")]
 		[TestCategory(
 			"Argument")]
+		[TestMethod]
 		public async Task SummarizeThrowOnArgumentNullAsync()
 		{
 			await Assert.ThrowsExceptionAsync<ArgumentNullException>(
 				() => this.client.SummarizeAsync(
 					default));
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="options"></param>
+		/// <returns></returns>
+		[TestCategory(
+			"Summarize")]
+		[TestCategory(
+			"BadRequest")]
+		[DataTestMethod]
+		[DynamicData(nameof(SummarizeThrowOnBadRequestData))]
+		public async Task SummarizeThrowOnBadRequestAsync(
+			KagiSummarizeOptions options)
+		{
+			await Assert.ThrowsExceptionAsync<KagiException>(
+				() => this.client.SummarizeAsync(
+					options));
 		}
 	}
 }
