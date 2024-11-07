ea40fc039fd3176fae2507b5503d5a51f41e27ff
Patchouli <self@patchouli.sh>
2024-11-06 19:18:45 -0500

    Add extension project files

---
+++ source/Kagi.Extensions.DependencyInjection/KagiServiceOptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Kagi
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public sealed class KagiServiceOptions
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		public Uri BaseUrl { get; set; } =
+			KagiService.DefaultBaseUrl;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public string ApiKey { get; set; }
+	}
+}
---
+++ source/Kagi.Extensions.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+using Microsoft;
+using Microsoft.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kagi
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public static class ServiceCollectionExtensions
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		private const string KagiHttpClientName =
+			"kagi";
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="services"></param>
+		/// <param name="optionsCallback"></param>
+		/// <returns></returns>
+		public static IHttpClientBuilder AddKagiHttpClient(
+			this IServiceCollection services,
+			Action<KagiServiceOptions> optionsCallback = default)
+		{
+			ArgumentNullException
+				.ThrowIfNull(
+					services,
+					nameof(services));
+
+			var options =
+				new KagiServiceOptions();
+
+			return services
+				.AddHttpClient<IKagiService, KagiService>(
+					KagiHttpClientName,
+					(httpClient) =>
+					{
+						// Configure the base URL for the client.
+						httpClient.BaseAddress =
+							options.BaseUrl;
+
+						// Configure the default request headers for the client.
+						httpClient.DefaultRequestHeaders.Authorization =
+							new AuthenticationHeaderValue(
+								"Bot",
+								options.ApiKey);
+					});
+		}
+	}
+}
