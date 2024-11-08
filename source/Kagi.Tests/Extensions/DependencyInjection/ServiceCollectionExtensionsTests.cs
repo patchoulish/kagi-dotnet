860f5217ac5e80f2c6f990fc20f24ca673507fd6
Patchouli <self@patchouli.sh>
2024-11-08 12:43:35 -0500

    Add dependency injection extension unit tests

---
+++ source/Kagi.Tests/Extensions/DependencyInjection/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+using Microsoft;
+using Microsoft.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.TestTools;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kagi.Extensions.DependencyInjection
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	[TestClass]
+	public sealed class ServiceCollectionExtensionsTests
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		[TestCategory(
+			"ServiceCollectionExtensions")]
+		[TestCategory(
+			"AddKagiHttpClient")]
+		[TestCategory(
+			"Argument")]
+		[TestMethod]
+		public void AddKagiHttpClientThrowOnArgumentNull()
+		{
+			Assert.ThrowsException<ArgumentNullException>(
+				() => ServiceCollectionExtensions
+					.AddKagiHttpClient(
+						default));
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		[TestCategory(
+			"ServiceCollectionExtensions")]
+		[TestCategory(
+			"AddKagiHttpClient")]
+		[TestCategory(
+			"Result")]
+		[TestMethod]
+		public void AddKagiHttpClient()
+		{
+			var services =
+				new ServiceCollection();
+
+			services.AddKagiHttpClient(
+				(options) =>
+				{
+					options.ApiKey =
+						Environment.GetEnvironmentVariable(
+							"KAGI_API_KEY");
+				});
+
+			var serviceProvider =
+				services.BuildServiceProvider();
+
+			var kagi =
+				serviceProvider.GetService<IKagiService>();
+
+			Assert.IsNotNull(
+				kagi);
+		}
+	}
+}
